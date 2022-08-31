52c3a65a951a13b8e39605ff6ff29b40ecff4f9c
WangXiaojie01 <1259488569@qq.com>
2022-08-31 20:58:50 +0800

    提交代码

--- errer.go
+++ errer.go
@@ -16,3 +16,19 @@ func (e *callerErr) Error() string {
 func CallerErr(caller, format string, args ...interface{}) error {
 	return &callerErr{caller, format, args}
 }
+
+type callerErr_new struct {
+	pack string 
+	caller string
+	format string
+	args   []interface{}
+}
+
+func (e *callerErr_new) Error() string {
+	fmtStr := fmt.Sprintf("call by: %s.%s,\nerror is : %s\n", e.pack, e.caller, e.format)
+	return fmt.Sprintf(fmtStr, e.args...)
+}
+
+func CallerErrNew(pack, caller, format string, args ...interface{}) error {
+	return &callerErr_new{pack: pack, caller: caller, format: format, args: args}
+}
